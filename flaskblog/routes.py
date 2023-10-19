3c6541a8a7b139a1556445e6589fa1c917983224
Varun Jain <login.at.noon@gmail.com>
2023-10-19 18:20:27 +0530

    Updated Files

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -111,18 +111,29 @@ def post(post_id):
 
 @app.route("/post/<post_id>/update",methods=['GET','POST'])
 @login_required
-def post(post_id):
+def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
-            post.title = form.title.data
-            post.content = form.content.data
-            db.session.commit()
-            flash('Your post has been updated!','success')
-            return redirect(url_for('post',post_id=post.id))
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!','success')
+        return redirect(url_for('post',post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
-        form.content.data=form.content
+        form.content.data = post.content
     return render_template('create_post.html',title='Update Post', form=form, legend='Update Post')
+
+@app.route("/post/<post_id>/delete",methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'Your post has been deleted!','success')
+    return redirect(url_for('home'))
