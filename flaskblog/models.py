335dd83a5a8bb5d5e2db16b7fb7ab1328b87ff12
Varun Jain <login.at.noon@gmail.com>
2023-10-24 02:30:27 +0530

    Updated Files

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from itsdangerous import URLSafeTimedSerializer as Serializer
-from flaskblog import db, login_manager, app
+from flask import current_app as app
+from flaskblog import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
